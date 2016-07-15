73c8d8d04d30d74954c32c60c3f9625879dc85a8
Timothy Wong <masterchung7@gmail.com>
2016-07-15 16:15:31 -0500

    Update
    Asks for column to check and more user firendly.

--- exceltest.py
+++ exceltest.py
@@ -1,18 +1,23 @@
+#changes to implement
 import openpyxl
 from openpyxl.styles import PatternFill
-#wb1 is 04-05
-#wb2 is 05-02
 #spreadsheet 1 is the old spreadsheet
 #spreadsheet 2 is the spreadsheet you want to identify new values in
-#wb1 = openpyxl.load_workbook('C:\\Users\\E065640\\Desktop\\work\\Private Prelim 05-02.xlsx')
-#sheet1 = wb1.get_sheet_by_name('Table 1')
-#wb2 = openpyxl.load_workbook('C:\\Users\\E065640\\Desktop\\work\\Private Prelim 07-07 (2).xlsx')
-#sheet2 = wb2.get_sheet_by_name('Table 1')
+
+def col2num(col_str):
+    expn = 0
+    col_num = 0
+    for char in reversed(col_str):
+        col_num += (ord(char.upper()) - ord('A') + 1) * (26 ** expn)
+        expn += 1
+    return col_num
 
 directory1 = input("Enter in the file path of the first excel sheet (Case sensitive): \n")
 sheetname1 = input("Enter in the name of the sheet: \n")
+column1 = col2num(input("Enter the column letter of the data: \n"))
 directory2 = input("Enter in the file path of the second excel sheet: \n")
 sheetname2 = input("Enter in the name of the sheet: \n")
+column2 = col2num(input("Enter the column letter of the data: \n"))
 wb1 = openpyxl.load_workbook(directory1)
 sheet1 = wb1.get_sheet_by_name(sheetname1)
 wb2 = openpyxl.load_workbook(directory2)
@@ -22,22 +27,21 @@ rgb=[255,255,0]
 color_string="".join([str(hex(x))[2:].upper().rjust(2, "0") for x in rgb])
 
 i=2 #start from the second row in the second sheet
-while (sheet2.cell(row=i, column=1).value != None):
+while (sheet2.cell(row=i, column = column1).value!= None) & (sheet2.cell(row=i+1, column = column1).value!= None):
     i=i+1
     #value = sheet2.cell(row=275, column=2).value
     j=2
     match = 0 #if match is equal to zero there are no matches. Set match equal to one if there is a match
-    while (sheet1.cell(row=j, column=1).value != None):
+    while (sheet1.cell(row=j, column = column2).value != None):
         j=j+1
-        if (sheet2.cell(row=i, column=2).value == sheet1.cell(row=j, column=2).value):
+        if (sheet2.cell(row=i, column = column2).value == sheet1.cell(row=j, column = column1).value):
             #print(str(i) + "," +str(j)+"\n")
             match = 1
     if match == 0:
         #print(sheet2.cell(row=i, column=1).value)
-        sheet2.cell(row=i, column=1).fill=PatternFill(fill_type="solid", start_color='FF' + color_string, end_color='FF' + color_string)
+        sheet2.cell(row=i, column=column1).fill=PatternFill(fill_type="solid", start_color='FF' + color_string, end_color='FF' + color_string)
 folderpath = '\\'.join(directory2.split('\\')[0:-1])
 #print(folderpath)
 filepath = folderpath + "\\out.xlsx"
-#wb2.save('C:\\Users\\E065640\\Desktop\\work\\test2.xlsx')
 #print(filepath)
 wb2.save(filepath)
