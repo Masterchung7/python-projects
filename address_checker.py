5f513a534b88d1588d35190eaa7797e243fdbb77
Timothy Wong <masterchung7@gmail.com>
2016-08-09 23:37:59 -0500

    Fixed long addresses, added multi address input
    Now handles addresses longer than 4 elements like 4558 S dr martin luther king jr dr
    Added ability to take in multiple addresses separated by comma like 4558 S dr martin luther king jr dr, 4550 S dr martin luther king jr dr

--- address_checker.py
+++ address_checker.py
@@ -1,14 +1,22 @@
 import requests
-x = (input("enter number of addresses to check: "))
 
-for i in range(int(x)):
-    street = input('enter a street address:\n')
-    streetparts = street.split(" ")
-
-    response = requests.get("https://gisapps.cityofchicago.org/kiosk/jsp/maptags.jsp?service=mapsplats_transonly&mapaction=address&adr_streetnum="+streetparts[0]+"+&adr_direction="+streetparts[1]+"&adr_streetname="+streetparts[2]+"&adr_streettype="+streetparts[3])
-    text = response.text
-    where = text.find('No location')
-    if text[where:where+11] == "No location":
-        print ("invalid address\n")
-    else:
-        print ("valid address\n")
+while True:
+    addresses = input('enter a street address:\n')
+    text = addresses.split(", ")
+    for i in range(len(text)):
+        print(text[i])
+        splittext = text[i].split()
+        address = "+".join(splittext[2:-1])
+        street = splittext[0] + " " + splittext[1] + " " + address + " " + splittext[-1]
+    
+        streetparts = street.split(" ")
+        response = requests.get("https://gisapps.cityofchicago.org/kiosk/jsp/maptags.jsp?service=mapsplats_transonly&mapaction=address&adr_streetnum="+streetparts[0]+"+&adr_direction="+streetparts[1]+"&adr_streetname="+streetparts[2]+"&adr_streettype="+streetparts[3])
+        webpage = response.text
+        where = webpage.find('No location')
+        if webpage[where:where+11] == "No location":
+            print ("invalid address\n")
+            #print ("https://gisapps.cityofchicago.org/kiosk/jsp/maptags.jsp?service=mapsplats_transonly&mapaction=address&adr_streetnum="+streetparts[0]+"+&adr_direction="+streetparts[1]+"&adr_streetname="+streetparts[2]+"&adr_streettype="+streetparts[3]+"\n")
+        else:
+            print ("valid address\n")
+            print ("https://gisapps.cityofchicago.org/kiosk/jsp/maptags.jsp?service=mapsplats_transonly&mapaction=address&adr_streetnum="+streetparts[0]+"+&adr_direction="+streetparts[1]+"&adr_streetname="+streetparts[2]+"&adr_streettype="+streetparts[3]+"\n\n")
+    
